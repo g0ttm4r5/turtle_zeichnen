1b347e5a5555dde5f6e8235871021a52317c28b2
GottMars <marsweller@gmx.de>
2022-03-25 14:16:38 +0100

    Update zeichnung.py
    change line 5
    change line 11

--- zeichnung.py
+++ zeichnung.py
@@ -2,10 +2,10 @@ import turtle
 
 pat = turtle. Turtle()
 
-for i in range(10):
+for i in range(15):
     for i in range (2):
         pat. forward (100)
         pat.right (60)
         pat. forward (100)
         pat.right (120)
-    pat.right (36)
+    pat.right (24)
